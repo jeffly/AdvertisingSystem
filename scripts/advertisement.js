435e7b69c9af8469a99fc538b7f22e906c66ebf7
CTlihaoliang <527549146@qq.com>
2014-09-25 16:33:58 +0800

    modify some function

--- scripts/advertisement.js
+++ scripts/advertisement.js
@@ -1,65 +1,70 @@
 /*
 name:广告组件
-autor:lihaoliang
+author:lihaoliang
 data:2014-08-04
 version:1.0
 */
   //UI展示部分
-  function AdvertView(target,option) {
+  function ShowBoxView(target,option) {
 
      this.config = $.extend({
         targetEle:"",    //目标元素
- 		    hoverEle:"", //鼠标hover元素
+        hoverEle:"", //鼠标hover元素
         contentEle:"",//存放广告元素
- 		    imageArray:[],//轮播的图片
- 	    	Speed:"1000",//滚动或者淡入淡出的速度
- 		    UnsidleTime:"2000" , //中间间隔时间
- 		    Method:1,//轮播的方式{1:淡入淡出，2：向上滚动，3，向左滚动，4：向右滚动}
- 		    Index:0,
- 		    First:0,//从哪一张开始
- 		    Len:"",
- 		    Width:780,
- 		    Height:58,
+        imageArray:[],//轮播的图片
+        Speed:"1000",//滚动或者淡入淡出的速度
+        UnsidleTime:"2000" , //中间间隔时间
+        Method:1,//轮播的方式{1:淡入淡出，2：向上滚动，3，向左滚动，4：向右滚动}
+        Index:0,
+        First:0,//从哪一张开始
+        Len:"",
+        Width:780,
+        Height:58,
         IsAdaptSize:false,
+        IsAutoClose:false,
+        ShowTime:30000,
         AdID:0
       },option || {});
 
      this.config.targetEle = target;
+     this.errorImages = [];
   }
-  AdvertView.prototype = {
-  	constructor:AdvertView,
+  ShowBoxView.prototype = {
+    constructor:ShowBoxView,
 
- 	    init: function() {
+      init: function() {
 
- 	    	this.config.Len = this.config.imageArray.length;
+        this.config.Len = this.config.imageArray.length;
         this.initDom();
         this.initStyle();
+        this.loadImage();
         this.bindEvent();
-   	  },
+      },
 
-   	  initDom: function() {
+      initDom: function() {
 
         var    arr = this.config.imageArray,
-   	        strTab = "<div class='m_tab tab'>",
-   	  	strContent = "<ul class='m_content' data-AdID= '"+this.config.AdID+"'>",
-   	  	         i = 0,
+            strTab = "<div class='m_tab tab'>",
+        strContent = "<ul class='m_content' data-AdID= '"+this.config.AdID+"'>",
+                 i = 0,
               href = "",
                src = "",
-   	  	       len = this.config.Len;
-   	  	for( ; i < len ; i++ ) {
+               len = this.config.Len;
+        for( ; i < len ; i++ ) {
              href = arr[i].Href || "javascript:void(0)";
               src = arr[i].Src  || "";
               AdIndex = arr[i] || 1;
              if(href.indexOf("http") == 0) {
-                 strContent += "<li data-Index='"+i+"'><a target='_blank' href='"+href+"' data-AdIndex= '"+(Number(arr[i].AdIndex)+1)+"' data-ImageID='"+arr[i].ImageID+"'><img src='"+src+"' /></a></li>";
+                 strContent += "<li data-Index='"+i+"'><a target='_blank' preSrc='"+arr[i].Src+"' href='"+href+"' data-AdIndex= '"+(Number(arr[i].AdIndex)+1)+"' data-ImageID='"+arr[i].ImageID+"'></a></li>";
              } else {
-                 strContent += "<li data-Index='"+i+"'><a href='"+href+"' data-AdIndex= '"+(Number(arr[i].AdIndex)+1)+"' data-ImageID='"+arr[i].ImageID+"'><img src='"+src+"' /></a></li>";
+                 strContent += "<li data-Index='"+i+"'><a preSrc='"+arr[i].Src+"' href='"+href+"' data-AdIndex= '"+(Number(arr[i].AdIndex)+1)+"' data-ImageID='"+arr[i].ImageID+"'></a></li>";
              }
             
-   	  	     strTab +="<span></span>";
-   	  	};
-   	  	    strContent +="</ul>";
-   	  	    strTab +="</div>";
+             strTab +="<span></span>";
+        };
+
+        strContent +="</ul>";
+        strTab +="</div>";
 
         this.config.targetEle.empty();
         this.config.targetEle.append(strContent);
@@ -67,51 +72,104 @@ version:1.0
         if(arr.length > 1) {
           this.config.targetEle.append(strTab);
         }
-        if(arr.length > 0) {
-           var tcmmStr = '<a  class="tcmm_ad" target="_blank" href="http://www.108mama.com/TcmmZone/AdPosition.aspx?AdpID='+this.config.AdID+'"  title="进入同城妈妈" ><b></b>'+
-                              '<span class="tcmm_text"><i></i><strong>同城妈妈提供的广告</strong></span></a>';
-           this.config.targetEle.append(tcmmStr);
-        }    
+        // if(arr.length > 0) {
+        //    var tcmmStr = '<a  class="tcmm_ad" target="_blank" href="http://www.108mama.com/TcmmZone/AdPosition.aspx?AdpID='+this.config.AdID+'"  title="进入同城妈妈" ><b></b>'+
+        //                       '<span class="tcmm_text"><i></i><strong>同城妈妈提供的广告</strong></span></a>';
+        //    this.config.targetEle.append(tcmmStr);
+        // }    
 
         this.config.hoverEle = this.config.targetEle.find('.m_tab');
         this.config.hoverEle.find("span").eq(this.config.Index).addClass("cur");
         this.config.contentEle = this.config.targetEle.find('.m_content');
         this.config.contentEle.append(this.config.contentEle.find("li[data-index=" + this.config.First + "]"));
-   	  },
+      },
 
-   	  initStyle: function(){
+      initStyle: function(){
 
         var   $content_a = this.config.contentEle.find('a'),
               $content_li = this.config.contentEle.find('li');
               this.config.hoverEle.css({"display":"none"});
 
-          if(this.config.IsAdaptSize) {
+         if(this.config.IsAdaptSize) {
                this.autoResizeImage();
-          }
+         }
+          
+          $content_a.css({width:this.config.Width+"px",height:this.config.Height+"px","line-height":this.config.Height+"px"});
 
-   	  	  if( this.config.Method == 1 ) {
-   	  	      $content_li.css({position:"absolute",top:"0px",left:"0px"});
-   	  	  	  this.config.contentEle.css({width:this.config.Width+"px",height:this.config.Height+"px",overflow:"hidden"});
-   	  	      this.config.targetEle.css({width:this.config.Width+"px",height:this.config.Height+"px"})
-   	          return;
-      	  } 
+          if( this.config.Method == 1 ) {
+              $content_li.css({position:"absolute",top:"0px",left:"0px"});
+              this.config.contentEle.css({width:this.config.Width+"px",height:this.config.Height+"px",overflow:"hidden"});
+              this.config.targetEle.css({width:this.config.Width+"px",height:this.config.Height+"px"})
+              return;
+          } 
 
           if( this.config.Method == 2 ) {
-             	  this.config.contentEle.css({position:"absolute",top:"0px",left:"0px"});  	            
+                this.config.contentEle.css({position:"absolute",top:"0px",left:"0px"});               
           } else if( this.config.Method == 3 ) {
                 $content_li.css({float:"left"});
                 this.config.contentEle.css({position:"absolute",width:this.config.Width*this.config.Len+"px",height:this.config.Height+"px"});
           } else if( this.config.Method == 4 ) {
                 $content_li.css({float:"right"});
-             	  this.config.contentEle.css({position:"absolute",width:this.config.Width*this.config.Len+"px",height:this.config.Height+"px"}); 
-          } 	else {
+                this.config.contentEle.css({position:"absolute",width:this.config.Width*this.config.Len+"px",height:this.config.Height+"px"}); 
+          }   else {
 
           };  
 
-          this.config.targetEle.css({width:this.config.Width+"px",height:this.config.Height+"px"});     		
-   	 
+          this.config.targetEle.css({width:this.config.Width+"px",height:this.config.Height+"px"});         
+     
 
-   	},
+    },
+
+    loadImage: function() {
+        var item =  this.config.targetEle.find("a");
+        var len = item.length;
+        var _this = this;
+        for(var i = 0 ; i < len ;i++) {
+
+            (function(i){
+                var $img = $(item[i]);
+                var _src = $img.attr("preSrc");
+                var img = new Image();
+
+                 img.onload = function () {
+                    $img.attr("background","");
+                    $img.append('<img src="'+_src+'"/>');
+                 };
+
+                 img.onerror = function () {
+                    var index = $(item[i]).closest('li').attr("data-index");
+                    _this.errorImages.push({"index":index,"src":_src});
+                    setTimeout(function(){
+                            _this.processErrorImages();
+                    },Math.max(_this.config.UnsidleTime*len,10000))
+                 };
+
+                 img.src = _src;
+
+            })(i);
+
+        }
+    },
+
+    processErrorImages: function () {
+          var arr = this.errorImages.slice(0),
+              len = arr.length,
+            _this = this,
+                i = 0;
+          for(; i < len ; i++) {
+             (function(i){
+                var img = new Image();
+
+                   img.onerror = function() {
+                        _this.config.imageArray.splice(arr[i].index,1);  
+                        _this.constructor.prototype.isError = true;
+                   }
+
+                   img.src = arr[i].src;
+                
+             })(i);
+          }
+    },
 
     autoResizeImage: function() {
           var _width = $(window).width(),
@@ -143,7 +201,7 @@ version:1.0
 
     },
 
- 	  start: function() {
+    start: function() {
 
         var _this=this;
         if ( this.config.Len <= 1) {
@@ -151,18 +209,22 @@ version:1.0
         }
 
         this.interval = setInterval(function() {
-     	       _this.config.Index++;
-     	       if( _this.config.Index >= _this.config.Len ) {
-     		        _this.config.Index = 0;
-     	       }
-       	    _this.change(_this.config.Index);
-         },this.config.UnsidleTime)
+             _this.config.Index++;
+             if( _this.config.Index >= _this.config.Len ) {
+                _this.config.Index = 0;
+             }
+            _this.change(_this.config.Index);
+         },this.config.UnsidleTime);
+
+        if(this.config.IsAutoClose) {
+           setTimeout("window.close()", this.config.ShowTime);
+        }
     },
 
     change: function(index) {
-
-    	 this.config.hoverEle.find("span").eq(index).addClass("cur").siblings().removeClass("cur");
-    	 
+       var _this = this;
+       this.config.hoverEle.find("span").eq(index).addClass("cur").siblings().removeClass("cur");
+       
        switch(this.config.Method) {
           case 1:  
              var _ele = this.config.contentEle.find("li[data-index=" + index + "]");
@@ -180,26 +242,45 @@ version:1.0
                this.congfig.contentEle.animate({ "right":"-"+index*100+"%"}, this.config.Speed);
                break;
           default: 
-    	 	       this.congfig.contentEle.animate({ "left":"-"+index*100+"%"}, this.config.Speed);
-    	 }
+               this.congfig.contentEle.animate({ "left":"-"+index*100+"%"}, this.config.Speed);
+       };
+
+       if(this.isError) {
+          if(index == this.config.Len-1) {
+             _this.stop();
+            setTimeout(function(){
+               _this.reInit();
+            },this.config.UnsidleTime)
+          }
+       }
+       
     },
 
-	  goto: function(index) {
+    goto: function(index) {
 
-	    this.config.Index = index;
+      this.config.Index = index;
         this.change(index);
- 	  },
+    },
 
- 	  stop: function() {
+    stop: function() {
 
- 		  if( this.interval ) {
- 			  clearInterval(this.interval);
- 		  }    
-   	},
+      if( this.interval ) {
+        clearInterval(this.interval);
+      }    
+    },
 
- 	  bindEvent: function() {
+    reInit: function() {
+        this.errorImages.splice(0,this.errorImages.length);
+        this.isError = false;
+        this.config.Index = 0;
+        this.init();
+        this.start(); 
+        
+    },
 
- 	  	var _this=this;
+    bindEvent: function() {
+
+      var _this=this;
       this.config.targetEle.mouseenter(function(event) {
           _this.config.hoverEle.show();
       });
@@ -221,16 +302,16 @@ version:1.0
         $.getJSON("http://www.108mama.com/public/TcyAdClickAjax.aspx?adpID="+adpID+"&AdChildBit="+AdChildBit+"&boardID="+boardID+"&sourceUrl="+document.URL+"&format=json&jsoncallback=?", function(data) { });
       });
 
-      this.config.targetEle.find(".tcmm_ad").hover(function(){
-        $('.tcmm_text').css("display","block");
-      },function(){
-        $('.tcmm_text').css("display","none");
-      })
+      // this.config.targetEle.find(".tcmm_ad").hover(function(){
+      //   $('.tcmm_text').css("display","block");
+      // },function(){
+      //   $('.tcmm_text').css("display","none");
+      // })
      }
    };
 
 //业务逻辑部分,输出最终数据
- function AdvertModel(data){
+ function ShowBoxModel(data){
     this.source = data;
     this.targetSource = [];
     this.buffer = null;
@@ -241,12 +322,14 @@ version:1.0
     this.UnsidleTime = data.UnsidleTime;
     this.serverTime = data.Time;
     this.IsAdaptSize = data.IsAdaptSize;
+    IsAutoClose = data.IsAutoClose;
     this.AdID = data.AdID;
     this.init();
  }
- AdvertModel.prototype = {
 
-   constructor: AdvertModel,
+ ShowBoxModel.prototype = {
+
+   constructor: ShowBoxModel,
 
    init: function () {
 
@@ -257,6 +340,12 @@ version:1.0
 
    filterData: function(_time) {//根据时间过滤数据
      this.targetSource = [];
+     var preTime = 0;
+     if(window.location.search.indexOf("=") != -1) {
+          var index = window.location.search.indexOf("=");
+           preTime = parseInt(window.location.search.substring(index+1)) * 60 * 60 * 1000; 
+     }
+
      var bufferTime = ( _time && _time*1000+100000 || 0 );
       if( typeof this.source == "undefined" ) return;
         var data = this.source.List,
@@ -267,7 +356,7 @@ version:1.0
              var starttime = this.formatDateToMillisecond(data[i].StartTime);
              var endtime = this.formatDateToMillisecond(data[i].EndTime);
            //  var  time = this.formatDateToMillisecond(this.serverTime);
-             var time = new Date().getTime() + bufferTime;
+             var time = new Date().getTime() + bufferTime  + preTime;
 
              if( time >= starttime && time < endtime ) {
                _data.push(data[i]);
@@ -334,7 +423,7 @@ version:1.0
      this.doInSertDefaultPisitoinData(); 
    },
 
-   doBuiltNormal:function(arr){  
+   doBuiltNormal:function(arr) {  
 
         var len = this.source.MaxNum;
         var arr = arr;
@@ -372,9 +461,9 @@ version:1.0
 
     var pos = this.source.MaxShowNum-1;
     var num = this.source.MaxNum -this.source.MaxShowNum;
-    var temp = this.targetSource.splice(pos);
+    var temp = this.targetSource.splice(pos,this.targetSource.length-pos);
 
-    for( var j = temp.length ; j >= 0 ; j-- ) {//删除undefined的对象
+    for( var j = temp.length -1 ; j >= 0 ; j-- ) {//删除undefined的对象
       if( typeof temp[j] == "undefined" ) {
         temp.splice(j,1);
       }
@@ -405,7 +494,8 @@ version:1.0
         }
       }
     };
-    for(var k = this.targetSource.length; k >= 0; k--) {
+    
+    for(var k = this.targetSource.length - 1; k >= 0; k--) {
       if(typeof this.targetSource[k] == "undefined") {
           this.targetSource.splice(k,1);
       }
@@ -436,7 +526,7 @@ version:1.0
 
     this.formatFinalData();
    },
-
+   
    randomArray: function() {
 
       var i = this.targetSource.length;
@@ -447,6 +537,7 @@ version:1.0
       };
 
       this.targetSource = temp;
+
    },
 
    formatFinalData:function() {
@@ -457,14 +548,15 @@ version:1.0
         Method: this.Method,
         UnsidleTime: this.UnsidleTime,
         IsAdaptSize:this.IsAdaptSize,
-        AdID:this.AdID
+        AdID:this.AdID,
+        IsAutoClose:this.IsAutoClose
      };
    }
 
  }
 
  //控制模块
- function AdvertController(model,view,updateTime){ 
+ function ShowBoxController(model,view,updateTime){ 
     this._model = model;
     this._view = view;
     this.updateTime = updateTime || 60;
@@ -478,7 +570,7 @@ version:1.0
     this.isBuffered = false;
  }
 
- AdvertController.prototype = {
+ ShowBoxController.prototype = {
 
   init: function(){
       this.countDownTime();
@@ -512,10 +604,28 @@ version:1.0
   },
 
   preLoadImage: function(imageArray) {
+     var _this = this;
+     var  len = total = imageArray.length,
+          img = null,
+          i = 0;
+        for(; i < len ; i++) {
+            (function(i){
+             var img = new Image();
+                 img.onload = function () {
+                      if(i == len-1) {
+                         _this._model.buffer.imageArray = imageArray;
+                      }
+                 };
+                 img.onerror = function () {
+                     imageArray.splice(i,1);
+                     if(i == len-1) {
+                       _this._model.buffer.imageArray = imageArray;
+                     }
+                 };
+                 img.src = imageArray[i].Src;
 
-      for( var i = 0 ; i < imageArray.length ; i++ ) {
-        var img = new Image();
-            img.src = imageArray[i].src;
-      }
-  }
+            })(i);
+
+        }
+   }
 }
